6e438b5773f088c1b9b5934579b60f421596e734
vibeside <vibeside01@gmail.com>
2024-04-04 16:55:08 -0400

    Completed

--- StormyWeatherFix/StormyPlugin.cs
+++ StormyWeatherFix/StormyPlugin.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using LethalLib;
 using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using UnityEngine;
@@ -17,7 +18,7 @@ namespace StormyWeatherFix
         public const string modVersion = "37.87.37";
         public static ManualLogSource mls = null!;
         public static Hook? GrabbableObjectStartHook;
-        public static Hook? stormyWeatherHook;
+        public static Hook? GrabbableObjectDestroyHook;
         public static Hook? stormyWeatherUpdateHook;
         public static ILHook? stormyWeatherILHook;
         public static List<GrabbableObject> metalObjects = [];
@@ -25,30 +26,33 @@ namespace StormyWeatherFix
         public void Awake()
         {
             mls = base.Logger;
-            GrabbableObjectStartHook = new(typeof(GrabbableObject).GetMethod(nameof(GrabbableObject.Start),AccessTools.allDeclared),FixPatch);
-            //stormyWeatherUpdateHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.OnEnable),AccessTools.allDeclared),WeatherPatch);
-            stormyWeatherILHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.OnEnable),AccessTools.allDeclared),StormyILHook);
-            stormyWeatherUpdateHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.OnEnable),AccessTools.allDeclared),WeatherPatch);
+            GrabbableObjectStartHook = new(typeof(GrabbableObject).GetMethod(nameof(GrabbableObject.Start),AccessTools.allDeclared),GOBjectStartPatch);
+            stormyWeatherUpdateHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.Update), AccessTools.allDeclared), WeatherPatch);
+            stormyWeatherILHook = new(typeof(StormyWeather).GetMethod(nameof(StormyWeather.OnEnable), AccessTools.allDeclared), StormyILHook);
+            GrabbableObjectDestroyHook = new(typeof(RoundManager).GetMethod(nameof(RoundManager.DetectElevatorIsRunning), AccessTools.allDeclared),GOBjectDestroyPatch);
         }
-        public static void FixPatch(Action<GrabbableObject> orig, GrabbableObject self)
+        public static void GOBjectDestroyPatch(Action<RoundManager> orig,RoundManager self)
+        {
+            orig(self);
+            for(int i = 0; i < metalObjects.Count; i++)
+            {
+                if (metalObjects[i] == null)
+                {
+                    metalObjects.RemoveAt(i);
+                }
+            }
+        }
+        public static void GOBjectStartPatch(Action<GrabbableObject> orig, GrabbableObject self)
         {
                 if (self.itemProperties.isConductiveMetal)
                 {
                     metalObjects.Add(self);
                 }
+            orig(self);
         }
         public static void WeatherPatch(Action<StormyWeather> orig, StormyWeather self)
         {
             orig(self);
-            foreach(GrabbableObject obj in metalObjects)
-            {
-                if (obj == null)
-                {
-                    mls.LogInfo("null"); 
-                    continue;
-                }
-                mls.LogInfo(obj);
-            }
             self.metalObjects = metalObjects;
         }
         public static void StormyILHook(ILContext il)
@@ -56,10 +60,10 @@ namespace StormyWeatherFix
             ILCursor c = new(il);
 
             c.GotoNext(
-                x => x.MatchLdarg(0),
-                x => x.MatchLdarg(0),
-                x => x.MatchCall<StormyWeather>("GetMetalObjectsAfterDelay")
-                );
+               x => x.MatchLdarg(0),
+               x => x.MatchLdarg(0),
+               x => x.MatchCall<StormyWeather>("GetMetalObjectsAfterDelay")
+               );
             c.RemoveRange(5);
         }
     }
